5e2fa793e200b40dc521f9c165b12e5fae9c75c1
luoguliziheng <liziheng@masteroj.cn>
2020-05-04 15:33:37 +0800

    Site updated: 2020-05-04 15:33:31

--- sw.js
+++ sw.js
@@ -28,25 +28,25 @@ var precacheUrls = [];
 
   precacheUrls.push('/love-helps-unite-to-fight-against-epidemic-disease.html');
 
-  precacheUrls.push('/op2006z.html');
-
   precacheUrls.push('/personal-homepage.html');
 
   precacheUrls.push('/test-encryption.html');
 
-  precacheUrls.push('/solution-cf132e.html');
+  precacheUrls.push('/solution-cf28a.html');
 
   precacheUrls.push('/solution-cf23c.html');
 
-  precacheUrls.push('/solution-cf28a.html');
+  precacheUrls.push('/op2006z.html');
 
-  precacheUrls.push('/solution-p5462.html');
-
-  precacheUrls.push('/solution-sp1676.html');
+  precacheUrls.push('/solution-uva1591.html');
 
   precacheUrls.push('/solution-sp2881.html');
 
-  precacheUrls.push('/solution-uva1591.html');
+  precacheUrls.push('/solution-cf132e.html');
+
+  precacheUrls.push('/solution-sp1676.html');
+
+  precacheUrls.push('/solution-uva571.html');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":30};
