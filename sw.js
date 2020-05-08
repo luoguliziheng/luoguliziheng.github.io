b152c9c82644c49a3c2b1db2b4737694692bf8b2
luoguliziheng <liziheng@masteroj.cn>
2020-05-08 18:25:45 +0800

    Site updated: 2020-05-08 18:25:40

--- sw.js
+++ sw.js
@@ -28,23 +28,23 @@ var precacheUrls = [];
 
   precacheUrls.push('/love-helps-unite-to-fight-against-epidemic-disease.html');
 
-  precacheUrls.push('/op2006z.html');
-
   precacheUrls.push('/personal-homepage.html');
+
+  precacheUrls.push('/t1.html');
 
   precacheUrls.push('/test-encryption.html');
 
-  precacheUrls.push('/solution-cf132e.html');
-
-  precacheUrls.push('/solution-cf23c.html');
+  precacheUrls.push('/op2006z.html');
 
   precacheUrls.push('/solution-cf28a.html');
 
-  precacheUrls.push('/solution-p1173.html');
+  precacheUrls.push('/solution-cf23c.html');
 
   precacheUrls.push('/solution-p5462.html');
 
-  precacheUrls.push('/solution-sp1676.html');
+  precacheUrls.push('/solution-cf132e.html');
+
+  precacheUrls.push('/solution-p1173.html');
 
   precacheUrls.push('/solution-sp2881.html');
 
